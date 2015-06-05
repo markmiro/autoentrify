e170bce6b8bfcef4644ce5e47f8274c6be4eaf00
CI-Server <ci@example.com>
2015-06-05 00:38:53 -0700

    added watching for mocha tests

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var browserify  = require('browserify'),
     gutil = require('gulp-util'),
     addSrc = require('gulp-add-src'),
     jest = require('jest-cli'),
-    tap = require('gulp-tap');
+    tap = require('gulp-tap'),
+    mocha = require('gulp-mocha');
 
 var autoEntrify = require('./index');
 
@@ -14,4 +15,14 @@ var autoEntrify = require('./index');
 gulp.task('default', function () {
   autoEntrify('test/*.html');
   return;
+});
+
+gulp.task('mocha', function() {
+    return gulp.src(['test.js'], { read: false })
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', gutil.log);
+});
+
+gulp.task('watch-mocha', function() {
+    gulp.watch(['*.js', '!gulpfile.js', 'testdir/*'], ['mocha']);
 });
